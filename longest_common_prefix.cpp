8ebfb85bf173bc05824094371dcf827b5b8678cf
Sameekshashetty30 <sameekshashetty3074@gmail.com>
2025-08-11 18:59:06 +0530

    Add files via upload

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+         string pref=strs[0];
+         int preflen=pref.length();
+        for(int i=1;i<strs.size();i++){
+            string s=strs[i];
+            
+              while(preflen>s.length()||pref!=s.substr(0,preflen)){
+          preflen--;
+          if(preflen==0)  
+              return "";
+          pref=pref.substr(0,preflen);
+        } 
+    }
+        return pref;
+    }
+};
